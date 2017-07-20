a9d3ded69bf3e8d0630f0efec5fb03644b839ea1
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-20 09:14:39 +0300

    An application structure has been created.

--- app/src/main/java/com/example/lynnik/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/com/example/lynnik/nerdlauncher/NerdLauncherActivity.java
@@ -1,13 +1,11 @@
 package com.example.lynnik.nerdlauncher;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 
-public class NerdLauncherActivity extends AppCompatActivity {
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_nerd_launcher);
+  protected Fragment createFragment() {
+    return NerdLauncherFragment.newInstance();
   }
 }
---
+++ app/src/main/java/com/example/lynnik/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,35 @@
+package com.example.lynnik.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NerdLauncherFragment extends Fragment {
+
+  private RecyclerView mRecyclerView;
+
+  public static NerdLauncherFragment newInstance() {
+    return new NerdLauncherFragment();
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View v = inflater
+        .inflate(R.layout.fragment_nerd_launcher, container, false);
+
+    mRecyclerView = (RecyclerView)
+        v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    return v;
+  }
+}
